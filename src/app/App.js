e9501f7d7f2ac3f947b0225b727c20f9fa7a2096
Abrahym Sharf Eldden <asharfeldden@gmail.com>
2020-06-17 18:00:23 -0400

    An annoyed commit

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
 import RoutesContainer from "../components/routes/RoutesContainer";
+import NavBarContainer from "../components/containers/NavBarContainer";
 class App extends Component {
   render() {
     return (
       <div className="App">
+ 		<NavBarContainer />
         <header className="App-header">
           <RoutesContainer/>
         </header>
