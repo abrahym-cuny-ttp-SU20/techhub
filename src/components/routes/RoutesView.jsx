cc8bae09c07e53eb71bb82083f352dcb50c8726b
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-06-19 18:09:35 +0100

    Created PrivateRoute to prevent access unless logged in.

--- src/components/routes/RoutesView.jsx
+++ src/components/routes/RoutesView.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import * as Container from "../containers";
+import PrivateRoute from "./PrivateRoute";
 
-const RoutesView = () => {
+const RoutesView = (props) => {
   return (
     <Switch>
-      <Route exact path="/" component={Container.HomePage} />
-      <Route exact path="/about" component={Container.AboutPage} />
-      <Route exact path="/jobs" component={Container.JobsPage} />
-      <Route exact path="/login" component={Container.LoginPage} />
-      <Route exact path="/signup" component={Container.SignupPage} />
-      <Route exact path="/edit" component={Container.EditUserProfilePage} />
+      {props.checked && (
+        <>
+          <Route exact path="/" component={Container.HomePage} />
+          <Route exact path="/about" component={Container.AboutPage} />
+          <Route exact path="/jobs" component={Container.JobsPage} />
+          <Route exact path="/login" component={Container.LoginPage} />
+          <Route exact path="/signup" component={Container.SignupPage} />
+          <PrivateRoute
+            exact
+            path="/edit"
+            component={Container.EditUserProfilePage}
+            authenticated={props.authenticated}
+          />
+        </>
+      )}
     </Switch>
   );
 };
