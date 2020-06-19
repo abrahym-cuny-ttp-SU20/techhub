86cc8e34851b53ae4a03de552e9963d32747e6fd
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-06-19 22:44:55 +0100

    Cleaned up login and logout thunks and added new update User session thunk.
    Update user session will be used to update just the user that is currently signed in.

--- src/store/utilities/session.js
+++ src/store/utilities/session.js
@@ -4,33 +4,43 @@ import { sessionService } from "redux-react-session";
 /**
  * THUNK CREATORS
  */
-export const loginThunk = (user, ownProps) => {
-    console.log(ownProps);
-  return () => {
-    return axios.post(`/api/users/login`, user).then((res) => {
-      const { id } = res.data.id;
-      sessionService.saveSession({ id }).then(() => {
-          sessionService.saveUser(res.data).then(() => {
-              ownProps.history.push("/");
-            })
-            .catch((err) => console.error(err));
-        })
-        .catch((err) => console.error(err));
-    });
-  };
+export const loginThunk = (user, ownProps) => () => {
+  return axios.post(`/api/users/login`, user).then((res) => {
+    const { id } = res.data.id;
+    sessionService
+      .saveSession({ id })
+      .then(() => {
+        sessionService
+          .saveUser(res.data)
+          .then(ownProps.history.push("/"))
+          .catch((err) => console.error(err));
+      })
+      .catch((err) => console.error(err));
+  });
 };
 
 export const logoutThunk = (history) => {
-    console.log(history);
   return () => {
-    return axios.post(`/api/users/logout`).then(() => {
-        sessionService.deleteSession().then(() =>
-            sessionService.deleteUser()).then(() =>
-            history.push("/"))
+    return axios
+      .post(`/api/users/logout`)
+      .then(() => {
+        sessionService
+          .deleteSession()
+          .then(() => sessionService.deleteUser())
+          .then(() => history.push("/"))
           .catch((err) => console.error(err));
       })
       .catch((err) => {
         throw err;
       });
   };
+};
+
+export const updateUserSessionThunk = (user) => () => {
+  return axios
+    .put(`/api/users`, user)
+    .then((res) => {
+      sessionService.saveUser(res.data);
+    })
+    .catch((err) => console.error(err));
 };
