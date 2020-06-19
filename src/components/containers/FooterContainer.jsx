0045723e3f2e080d0d937543876b64d5926c309a
Abrahym Sharf Eldden <asharfeldden@gmail.com>
2020-06-18 21:22:28 -0400

    Footer Styling setup

---
+++ src/components/containers/FooterContainer.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { FooterView } from "../views";
+
+class FooterContainer extends Component {
+  render() {
+    return <FooterView />;
+  }
+}
+
+export default FooterContainer;
---
+++ src/components/views/FooterView.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const FooterView = () => {
+	return (
+		<div className="footer-container">
+            <div className="left-align image-container">
+                <img src="#" />
+            </div>
+            <div className="right-align text-container">
+                <ul>
+                    <li>Icon1</li>
+                    <li>Icon2</li>
+                    <li>Icon3</li>
+                </ul>
+            </div>
+        </div> 
+	)
+}
+
+export default FooterView;
