94fece89d79d36708683680be31a5cfd962aa652
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-06-19 22:48:36 +0100

    Commented out app.css so that it does not interfere with styling of profile page.

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './App.css';
+//import './App.css';
 import RoutesContainer from "../components/routes/RoutesContainer";
 import { NavBar } from "../components/containers";
 
