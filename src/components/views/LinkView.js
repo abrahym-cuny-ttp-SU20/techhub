16c0a7792d1ddc4ba30fbd4c9d29af92ba82392b
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-06-19 12:44:22 +0100

    Added structure for linkView.

--- src/components/views/LinkView.js
+++ src/components/views/LinkView.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React from "react";
 import { Paper, Switch } from "@material-ui/core";
+import { DeleteForeverIcon } from "@material-ui/icons";
 
 const LinkView = (props) => {
-    return (<Paper className="link-container">
-        <div className="logo-img">
-            <img src="https://via.placeholder.com/150" alt=""/>
+  return (
+    <Paper className="link-container">
+      <div className="logo-img">
+        <img src="https://via.placeholder.com/150" alt="" />
+      </div>
+      <div className="link-inputs">
+        <div className="link-title-container">
+          <input type="text" placeholder="Enter title" />
         </div>
-        <div className="link-inputs">
-            <div className="link-title-container">
-                <input type="text" placeholder="Enter title"/>
-            </div>
-            <div className="link-url-container">
-                <input type="text" placeholder="http://url"/>
-            </div>
+        <div className="link-url-container">
+          <input type="text" placeholder="http://url" />
         </div>
-        <div className="option-buttons">
-            <div className="link-enable-switch">
-                <Switch
-                    name="checkedB"
-                    color="primary"
-                />
-            </div>
+      </div>
+      <div className="option-buttons">
+        <div className="link-enable-switch">
+          <Switch name="checkedB" color="primary" />
         </div>
-    </Paper>)
-}
+          <div className="delete-link-button">
+              <DeleteForeverIcon color="secondary"/>
+          </div>
+      </div>
+    </Paper>
+  );
+};
 export default LinkView;
