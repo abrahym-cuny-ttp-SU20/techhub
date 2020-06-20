282c8d1ee37758e60fe50e8634b22cf31394cefe
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-06-20 12:53:12 +0100

    Changed links to pageLinks to match what is on database.

--- src/thunks/index.js
+++ src/thunks/index.js
@@ -1,4 +1,4 @@
 // Barrel file for thunks, which will be mapped to and invoked within our smart containers;
 export * from "../store/utilities/user";
 export * from "../store/utilities/session";
-export * from "../store/utilities/links";
+export * from "../store/utilities/pageLinks";
