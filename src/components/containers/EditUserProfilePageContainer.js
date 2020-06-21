da9b1cb489b2fe4060f13a720213c72f4c096b3b
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-06-21 14:34:37 +0100

    Added skills and page links to edit profile page.
    Still need to attach data from backend to skills.

--- src/components/containers/EditUserProfilePageContainer.js
+++ src/components/containers/EditUserProfilePageContainer.js
@@ -6,16 +6,34 @@ import {
   updateUserSessionThunk,
   fetchUserPageLinksThunk,
   editPageLinkThunk,
+  deletePageLinkThunk,
+  addPageLinkThunk,
 } from "../../thunks";
 
 class EditUserProfilePageContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      skillInput: '',
       pageLinks: [],
+      skills: [
+        {
+          id: 1,
+          name: "React",
+        },
+        {
+          id: 2,
+          name: "Angular",
+        },
+        {
+          id: 3,
+          name: "Javascript",
+        },
+      ],
     };
     this.handleChange = this.handleChange.bind(this);
   }
+
   componentDidMount() {
     this.setState({ user: this.props.user });
     this.props
@@ -24,7 +42,6 @@ class EditUserProfilePageContainer extends Component {
   }
 
   handleChange = (e, id) => {
-    console.log(e.target.value, id);
     this.setState({
       pageLinks: this.state.pageLinks.map((pageLink) =>
         pageLink.id === id
@@ -34,18 +51,76 @@ class EditUserProfilePageContainer extends Component {
     });
   };
 
+  handleSkillChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  handleDeletePageLink = (e, id) => {
+    this.props
+      .deletePageLink(id)
+      .then(() => this.props.fetchUserPageLinks(this.props.user.id))
+      .then(({ payload }) => this.setState({ pageLinks: payload }));
+  };
+
+  handleDeleteSkill = (skillToDelete) => () => {
+    this.setState((state) => {
+      return {
+        skills: state.skills.filter((skill) => skill.id !== skillToDelete.id),
+      };
+    });
+  };
+
   handleSavePageLink = (e, id) => {
     e.preventDefault();
-    const pageLink = this.state.pageLinks.filter((pageLink) => pageLink.id === id);
+    const pageLink = this.state.pageLinks.filter(
+      (pageLink) => pageLink.id === id
+    );
     this.props.editPageLink(...pageLink);
+  };
+
+  handleAddPageLink = (e) => {
+    const pageLink = {
+      link: "",
+      imageURL: "",
+      title: "",
+      userId: this.props.user.id,
+    };
+    this.props.addPageLink(pageLink).then(({ payload }) => {
+      console.log(payload);
+      this.setState((prevState) => {
+        return {
+          pageLinks: [payload, ...prevState.pageLinks],
+        };
+      });
+    });
+  };
+
+  handleAddSkill = (e) => {
+    const skill = {
+      id: 5,
+      name: this.state.skillInput,
+    };
+
+    this.setState((state) => {
+      return {
+        skills: [skill,...state.skills],
+      };
+    });
   };
 
   render() {
     return (
       <EditUserProfilePageView
-        pageLinks={this.state.pageLinks}
         handleSavePageLink={this.handleSavePageLink}
+        handleDeletePageLink={this.handleDeletePageLink}
+        handleAddPageLink={this.handleAddPageLink}
+        handleDeleteSkill={this.handleDeleteSkill}
+        handleAddSkill={this.handleAddSkill}
         handleChange={this.handleChange}
+        handleSkillChange={this.handleSkillChange}
+        user={this.props.user}
+        pageLinks={this.state.pageLinks}
+        skills={this.state.skills}
       />
     );
   }
@@ -54,7 +129,7 @@ class EditUserProfilePageContainer extends Component {
 const mapStateToProps = (state) => {
   return {
     user: state.session.user,
-    links: state.links,
+    pageLinks: state.pageLinks,
   };
 };
 
@@ -63,11 +138,17 @@ const mapDispatchToProps = (dispatch) => {
     updateUserSession: (user) => dispatch(updateUserSessionThunk(user)),
     fetchUserPageLinks: (id) => dispatch(fetchUserPageLinksThunk(id)),
     editPageLink: (pageLink) => dispatch(editPageLinkThunk(pageLink)),
+    deletePageLink: (id) => dispatch(deletePageLinkThunk(id)),
+    addPageLink: (pageLink) => dispatch(addPageLinkThunk(pageLink)),
   };
 };
 
 EditUserProfilePageContainer.propTypes = {
   user: PropTypes.object.isRequired,
+  fetchUserPageLinks: PropTypes.func.isRequired,
+  editPageLink: PropTypes.func.isRequired,
+  deletePageLink: PropTypes.func.isRequired,
+  addPageLink: PropTypes.func.isRequired,
 };
 
 export default connect(
--- src/components/views/EditUserProfilePageView.js
+++ src/components/views/EditUserProfilePageView.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { EditPageLinkContainer } from "../containers";
+import { Button, Chip, Paper, Input } from '@material-ui/core';
+import "./styles/EditUserProfilePageContainer.css";
 
 const EditUserProfilePageView = (props) => {
   const pageLinks = () =>
@@ -9,24 +11,46 @@ const EditUserProfilePageView = (props) => {
         {...pageLink}
         handleChange={props.handleChange}
         handleSavePageLink={props.handleSavePageLink}
+        handleDeletePageLink={props.handleDeletePageLink}
       />
     ));
   return (
     <div className="page">
       <div className="user-details">
-        <div className="profile-image"></div>
+        <div className="image-cropper">
+            <img className="profile-pic" src={props.user.profileImageURL} alt="user"/>
+        </div>
         <div className="qr-code"></div>
         <div className="skills">
-          <div className="add-skill-btn"></div>
-          <div>Skill Component goes here</div>
+          <div className="add-skill-btn">
+                  <Input placeholder="Enter Skill" name="skillInput" onChange={props.handleSkillChange} value={props.skillInput} inputProps={{ 'aria-label': 'description' }} />
+                  <Button color="primary" variant="contained" onClick={props.handleAddSkill}>Add Skill</Button>
+          </div>
+          <div>
+              <Paper component="ul" className="skill-container">
+              {props.skills.map((skill) => {
+                  return (
+                      <li key={skill.id}>
+                          <Chip
+                              label={skill.name}
+                              onDelete={props.handleDeleteSkill(skill)}
+                          />
+                      </li>
+                  );
+              })}
+          </Paper>
+          </div>
         </div>
       </div>
       <div className="links">
-        <div className="add-link-button"></div>
+        <div className="add-link-button-container">
+            <Button className="add-link-button" size="large" variant="contained" color="primary" onClick={props.handleAddPageLink}>
+                Add Link
+            </Button>
+        </div>
         {pageLinks()}
       </div>
     </div>
   );
 };
-
 export default EditUserProfilePageView;
